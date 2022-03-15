f1daa0b0e799387d2e842109deee95d29354bc8a
AHartNtkn <AHartNtkn@github.com>
2022-03-14 17:42:01 -0700

    Replace tabs with spaces.

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
           ];
         };
 
-	      codium = stdenv.mkDerivation {
+        codium = stdenv.mkDerivation {
                 pname = "codium";
                 version = "1.0";
                 phases = ["installPhase"];
