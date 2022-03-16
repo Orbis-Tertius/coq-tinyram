e9a5aa450cfa8f1a2b567e5133fa4ecb01ecabc9
matthewcroughan <matt@croughan.sh>
2022-03-16 21:31:04 +0000

    flake: format with nixpkgs-fmt

--- flake.nix
+++ flake.nix
@@ -12,6 +12,7 @@
     , nixpkgs
     , emacs
     , nix-doom-emacs
+    ,
     }:
     let
       # Generate a user-friendly version number.
@@ -21,54 +22,60 @@
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
+      nixpkgsFor = forAllSystems (system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
+        });
     in
     {
-      overlay = final: prev: {};
+      overlay = final: prev: { };
       devShells = forAllSystems (system:
-      let
-        pkgs = nixpkgsFor."${system}";
-      in
-      {
-        defaultShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            ocaml
-            dune_2
-            coqPackages.coq
-            coqPackages.coq-ext-lib
-            coqPackages.ITree
-          ];
-        };
-        vscodium = self.devShells.${system}.defaultShell.overrideAttrs (old: {
-          buildInputs =
-            let
-              vscodeWithCoq = pkgs.vscode-with-extensions.override {
-                vscode = pkgs.vscodium;
-                vscodeExtensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-                  {
-                    name = "VSCoq";
-                    publisher = "maximedenes";
-                    version = "0.3.6";
-                    sha256 = "sha256-b0gCaEzt5yAj53oLFZSXSD3bum9J1fYes/uf9+OlUek=";
-                  }
-                ];
-              };
-            in
-            old.buildInputs ++ [
-              vscodeWithCoq
+        let
+          pkgs = nixpkgsFor."${system}";
+        in
+        {
+          defaultShell = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              ocaml
+              dune_2
+              coqPackages.coq
+              coqPackages.coq-ext-lib
+              coqPackages.ITree
             ];
+          };
+          vscodium = self.devShells.${system}.defaultShell.overrideAttrs (old: {
+            buildInputs =
+              let
+                vscodeWithCoq = pkgs.vscode-with-extensions.override {
+                  vscode = pkgs.vscodium;
+                  vscodeExtensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+                    {
+                      name = "VSCoq";
+                      publisher = "maximedenes";
+                      version = "0.3.6";
+                      sha256 = "sha256-b0gCaEzt5yAj53oLFZSXSD3bum9J1fYes/uf9+OlUek=";
+                    }
+                  ];
+                };
+              in
+              old.buildInputs
+              ++ [
+                vscodeWithCoq
+              ];
           });
-        emacs = self.devShells.${system}.defaultShell.overrideAttrs (old: {
-          buildInputs =
-            let
-              doom-emacsWithCoq = nix-doom-emacs.package.${system} {
-                doomPrivateDir = ./nix/doom.d;
-              };
-            in
-            old.buildInputs ++ [
-              doom-emacsWithCoq
-            ];
+          emacs = self.devShells.${system}.defaultShell.overrideAttrs (old: {
+            buildInputs =
+              let
+                doom-emacsWithCoq = nix-doom-emacs.package.${system} {
+                  doomPrivateDir = ./nix/doom.d;
+                };
+              in
+              old.buildInputs
+              ++ [
+                doom-emacsWithCoq
+              ];
+          });
         });
-      });
     };
 }
