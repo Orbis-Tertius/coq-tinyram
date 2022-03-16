51d3f182217b30091b89ca366737e274c7d26812
matthewcroughan <matt@croughan.sh>
2022-03-16 22:59:29 +0000

    Add Hercules-CI

--- flake.nix
+++ flake.nix
@@ -2,6 +2,11 @@
   description = "A Flake for building coq and providing devShells for the coq-tinyram project";
 
   inputs = {
+    flake-compat-ci.url = "github:hercules-ci/flake-compat-ci";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
     emacs.url = "github:cmacrae/emacs";
@@ -12,7 +17,8 @@
     , nixpkgs
     , emacs
     , nix-doom-emacs
-    ,
+    , flake-compat
+    , flake-compat-ci
     }:
     let
       # Generate a user-friendly version number.
@@ -29,6 +35,10 @@
         });
     in
     {
+      ciNix = flake-compat-ci.lib.recurseIntoFlakeWith {
+        flake = self;
+        systems = [ "x86_64-linux" ];
+      };
       overlay = final: prev: { };
       # the default devShell used when running `nix develop`
       devShell = forAllSystems (system: self.devShells.${system}.defaultShell);
