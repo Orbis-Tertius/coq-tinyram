4373c3c1aa42a327dc8de5c76948f1c4388e34a2
matthewcroughan <matt@croughan.sh>
2022-03-16 21:28:37 +0000

    treewide: refactor Nix code

--- flake.nix
+++ flake.nix
@@ -1,33 +1,74 @@
 {
-  description = "Write coq in doom for the coq-tinyram project";
+  description = "A Flake for building coq and providing devShells for the coq-tinyram project";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
     emacs.url = "github:cmacrae/emacs";
-    flake-utils.url  = "github:numtide/flake-utils";
   };
 
   outputs =
     { self
     , nixpkgs
     , emacs
-    , flake-utils
     , nix-doom-emacs
     }:
-    flake-utils.lib.eachDefaultSystem (system:
+    let
+      # Generate a user-friendly version number.
+      version = builtins.substring 0 8 self.lastModifiedDate;
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
+    in
+    {
+      overlay = final: prev: {};
+      devShells = forAllSystems (system:
       let
-        overlays = [ (import emacs) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
+        pkgs = nixpkgsFor."${system}";
       in
-      with pkgs;
       {
-        defaultPackage = nix-doom-emacs.package.${system} {
-          doomPrivateDir = ./nix/doom.d;
+        defaultShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            ocaml
+            dune_2
+            coqPackages.coq
+            coqPackages.coq-ext-lib
+            coqPackages.ITree
+          ];
         };
-        shell = ./nix/coq.nix;
-      }
-    );
+        vscodium = self.devShells.${system}.defaultShell.overrideAttrs (old: {
+          buildInputs =
+            let
+              vscodeWithCoq = pkgs.vscode-with-extensions.override {
+                vscode = pkgs.vscodium;
+                vscodeExtensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+                  {
+                    name = "VSCoq";
+                    publisher = "maximedenes";
+                    version = "0.3.6";
+                    sha256 = "sha256-b0gCaEzt5yAj53oLFZSXSD3bum9J1fYes/uf9+OlUek=";
+                  }
+                ];
+              };
+            in
+            old.buildInputs ++ [
+              vscodeWithCoq
+            ];
+          });
+        emacs = self.devShells.${system}.defaultShell.overrideAttrs (old: {
+          buildInputs =
+            let
+              doom-emacsWithCoq = nix-doom-emacs.package.${system} {
+                doomPrivateDir = ./nix/doom.d;
+              };
+            in
+            old.buildInputs ++ [
+              doom-emacsWithCoq
+            ];
+        });
+      });
+    };
 }
