881fc43b63b99ae372850adcb2d27d8cf7d1b878
AHartNtkn <AHartNtkn@github.com>
2022-04-06 15:30:41 -0700

    simplify extraction imports

--- theories/Extraction.v
+++ theories/Extraction.v
@@ -1,19 +1,9 @@
 Require Coq.extraction.Extraction.
 Extraction Language Haskell.
 
-From Coq Require Import
-     Lia VectorDef VectorEq List.
-From TinyRAM.Utils Require Import
-     Fin BitVectors.
 From TinyRAM.Machine Require Import
-     Parameters Coding Handlers.
-Import PeanoNat.Nat VectorNotations.
-
-From ITree Require Import
-     ITree
-     ITreeFacts
-     Events.State
-     Events.StateFacts.
+     Parameters Handlers.
+Import PeanoNat.Nat.
 
 (* Example archetecture with 4 registers and a wordsize of 16 bits. *)
 Module SixteenFour <: TinyRAMParameters.
@@ -24,10 +14,11 @@ Module SixteenFour <: TinyRAMParameters.
   Theorem wordSizePow2 : wordSize = 2 ^ wordSizeLength.
   Proof. unfold wordSize. simpl. reflexivity. Qed.
   Theorem encodingAxiom : 6 + 2 * log2_up registerCount <= wordSize.
-  Proof. unfold registerCount. unfold wordSize. simpl. lia. Qed.
+  Proof. unfold registerCount. unfold wordSize. simpl.
+         repeat (apply le_S; try apply le_n). Qed.
 End SixteenFour.
 
 Module TRHand := TinyRAMHandlers SixteenFour.
 Import TRHand.
 
-Extraction "tinyram_vm.hs" eval_prog.
+(*Extraction "tinyram_vm.hs" eval_prog.*)
