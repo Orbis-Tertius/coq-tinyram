17e1be2dd8a341690b6ab05330c37a2177f65235
AHartNtkn <AHartNtkn@github.com>
2022-04-12 17:18:38 -0700

    make example program use words rather than bytes.

--- theories/ExamplePrograms.v
+++ theories/ExamplePrograms.v
@@ -33,8 +33,8 @@ Definition FibProgram : Program.
   (* Store 1 into address 0001 *)
     (*0: Store 1 into register 00 *)
   apply (cons (movI (bitvector_fin_big [b0; b0]), inl (nat_bitvector_big _ 1))).
-    (*1: Store [00] into address 0001 *)
-  apply (cons (store_bI (bitvector_fin_big [b0; b0]), inl (nat_bitvector_big _ 1))).
+    (*1: Store [00] at address 0001 *)
+  apply (cons (store_wI (bitvector_fin_big [b0; b0]), inl (nat_bitvector_big _ 1))).
 
   (*2: Read input from main tape into register 00. *)
   apply (cons (readI (bitvector_fin_big [b0; b0]), inl (nat_bitvector_big _ 0))).
@@ -47,9 +47,9 @@ Definition FibProgram : Program.
 
   (* Read two addresses into registers *)
   (*5: read address 0 into 01 *)
-  apply (cons (load_bI (bitvector_fin_big [b0; b1]), inl (nat_bitvector_big _ 0))).
-  (*6: read address 1 into 10  *)
-  apply (cons (load_bI (bitvector_fin_big [b1; b0]), inl (nat_bitvector_big _ 1))).
+  apply (cons (load_wI (bitvector_fin_big [b0; b1]), inl (nat_bitvector_big _ 0))).
+  (*6: read address 2 into 10  *)
+  apply (cons (load_wI (bitvector_fin_big [b1; b0]), inl (nat_bitvector_big _ 2))).
 
   (*7: add two registers together; [01] := [01] + [10] *)
   apply (cons (addI (bitvector_fin_big [b0; b1]) (bitvector_fin_big [b0; b1]),
@@ -57,9 +57,9 @@ Definition FibProgram : Program.
 
   (* Store both registers *)
   (*8: store [10] into adress 0 *)
-  apply (cons (store_bI (bitvector_fin_big [b1; b0]), inl (nat_bitvector_big _ 0))).
-  (*9: store [01] into adress 1 *)
-  apply (cons (store_bI (bitvector_fin_big [b0; b1]), inl (nat_bitvector_big _ 1))).
+  apply (cons (store_wI (bitvector_fin_big [b1; b0]), inl (nat_bitvector_big _ 0))).
+  (*9: store [01] into adress 2 *)
+  apply (cons (store_wI (bitvector_fin_big [b0; b1]), inl (nat_bitvector_big _ 2))).
 
   (*10: decriment [00] *)
   apply (cons (subI (bitvector_fin_big [b0; b0]) (bitvector_fin_big [b0; b0]),
