ceebbd0a4888133f13e208a22a195d7c5fd0847d
AHartNtkn <AHartNtkn@github.com>
2022-04-19 21:39:15 -0700

    finish initial extraction script.

--- theories/Extraction.v
+++ theories/Extraction.v
@@ -1,9 +1,11 @@
 Require Coq.extraction.Extraction.
 Extraction Language Haskell.
 
+Require Import ExtrHaskellBasic.
+
 From TinyRAM.Machine Require Import
      Parameters Handlers.
-Import PeanoNat.Nat.
+Import PeanoNat.Nat BinNat.
 
 (* Example archetecture with 4 registers and a wordsize of 16 bits. *)
 Module SixteenFour <: TinyRAMParameters.
@@ -21,4 +23,24 @@ End SixteenFour.
 Module TRHand := TinyRAMHandlers SixteenFour.
 Import TRHand.
 
-(*Extraction "tinyram_vm.hs" interp_program.*)
+Extract Inductive Datatypes.nat => "Prelude.Integer" ["0" "succ"]
+ "(\fO fS n -> if n Prelude.== 0 then fO () else fS (n Prelude.- 1))".
+Extract Inductive positive => "Prelude.Integer" [ "((1 Prelude.+) GHC.Base.. (2 Prelude.*))" "(2 Prelude.*)" "1" ]
+ "(\fxI fxO xH n -> if n Prelude.== 1 then xH () else 
+    ( if (Prelude.mod n 2) Prelude.== 1
+      then fxI (Prelude.div (n Prelude.- 1) 2)
+      else fxO (Prelude.div n 2)
+    )
+  )".
+Extract Inductive N => "Prelude.Integer" [ "0" "GHC.Base.id" ] 
+ "(\fZ fP n -> if n Prelude.== 0 then fZ () else fP n)".
+Extract Inductive Z => "Prelude.Integer" [ "0" "Prelude.toInteger" "((0 Prelude.-) GHC.Base.. Prelude.toInteger)" ]
+ "(\fZ fP fN n -> if n Prelude.== 0 then fZ () else 
+    ( if n Prelude.> 0
+      then fP n
+      else fN (Prelude.abs n)
+    )
+  )".
+Extract Inductive comparison => "Prelude.Ordering" [ "Prelude.EQ" "Prelude.LT" "Prelude.GT" ].
+
+Extraction "tinyram_vm.hs" interp_program.
